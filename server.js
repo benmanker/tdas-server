99c78ef876332c9ef3803a3d37660ff02951c7a7
Ben Manker <benmanker@gmail.com>
2023-03-14 01:37:48 -0500

    added routes

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const connectDatabase = require("./config/connectDatabase");
+const connectDatabase = require("./config/databaseConnection");
 
+const BASE_URL = "/api";
 const PORT = 3001;
 
-connectDatabase();
+// built-in middleware for json
+app.use(express.json());
 
+// endpoint for testing (http://localhost:3001/api/ in browser)
+app.get(BASE_URL, (res) => {
+  res.send("Hello World!");
+});
+
+// routes
+app.use(BASE_URL + "/data", require("./routes/data"));
+
+// connect to database and start express server
+connectDatabase();
 mongoose.connection.once("connected", () => {
-  console.log("[INFO] Database connection was successful");
+  console.log("[INFO] Database connection successful");
   app.listen(PORT, () =>
-    console.log(`[INFO] Server is running on port ${PORT}`)
+    console.log(`[INFO] Server accessible at http://localhost:${PORT}/api/`)
   );
 });
